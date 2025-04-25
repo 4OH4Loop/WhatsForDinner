2c72fecfd8a59aad1be34017afe571f2d0f6a9c9
4OH4Loop <semperchuck@gmail.com>
2025-04-25 16:17:29 -0400

    Initial Commit

---
+++ WhatsForDinner/WhatsForDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsForDinnerApp.swift
+//  WhatsForDinner
+//
+//  Created by Carolyn Ballinger on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatsForDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
